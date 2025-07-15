84ec27a750ca8bf90063ffc115c688b7fd9f1ba7
Pratyaksh Yadav <ypratyaksh59@gmail.com>
2025-07-15 17:12:58 +0530

    Update gemini_api.py

--- utils/gemini_api.py
+++ utils/gemini_api.py
@@ -1,10 +1,9 @@
 import requests
 import base64
-import os
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from config import GEMINI_API_KEY
+from secrets_config import GEMINI_API_KEY
 
 
 if not GEMINI_API_KEY:
